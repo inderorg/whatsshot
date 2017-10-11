e83af25760630b770d8327c601fdb1e114397c11
inder.patliya <inder.patliya@247-inc.com>
2017-10-11 16:26:08 +0530

    2nd commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome</h1>
+        </header>
+        <div className="App-intro">
+          Progressive Web Apps<br/>
+          To get started, edit <code>src/App.js</code> and save to reload.<br/>
+          <span float='left'> $ heroku login </span><br/>
+          <span> $git clone https://github.com/heroku/node-js-getting-started.git</span><br/>
+          <span> $cd node-js-getting-started </span><br/>
+          <span> $heroku create </span><br/>
+          <span> $git push heroku master</span><br/>
+          <span> $heroku ps:scale web=1 </span><br/>
+          <span> $heroku open </span><br/>
+          <span> $heroku logs --tail  --to view logs</span><br/>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
